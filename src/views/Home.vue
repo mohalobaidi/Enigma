dbcfc890cc0d1cd8f897c67c2c816b68479b331f
Mohammed Alobaidi <contact@mohalobaidi.com>
2019-10-18 21:59:30 +0300

    Hide panes if there are no challenges

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,15 @@
 #Home
   .container
     Header
-    Pane(title="Continue Solving")
+    Pane(title="Continue Solving" v-if="inProgressChallenges.length")
       Grid(:cols="4")
-        ChallengeCard( v-for="(challenge, i) in challenges.slice(0, 3)" :key="i" v-bind="challenge")
-    Pane(title="All Challenges")
+        ChallengeCard( v-for="(challenge, i) in inProgressChallenges" :key="i" v-bind="challenge")
+    Pane(title="All Challenges" v-if="newChallenges.length")
       Grid(:cols="4")
-        ChallengeCard( v-for="(challenge, i) in challenges.slice(3)" :key="i" v-bind="challenge")
-    Pane(title="Solved Challenges")
+        ChallengeCard( v-for="(challenge, i) in newChallenges" :key="i" v-bind="challenge")
+    Pane(title="Solved Challenges" v-if="solvedChallenges.length")
       Grid(:cols="4")
-        ChallengeCard( v-for="(challenge, i) in challenges.slice(0, 6)" :key="i" v-bind="challenge")
+        ChallengeCard( v-for="(challenge, i) in solvedChallenges" :key="i" v-bind="challenge")
 </template>
 
 <script>
@@ -20,7 +20,18 @@ import { mapGetters } from 'vuex'
 // TODO: WRITE A REAL TODO, or just fix the code.
 export default {
   name: 'Home',
-  computed: { ...mapGetters(['challenges', 'user']) },
+  computed: {
+    ...mapGetters(['challenges', 'user']),
+    inProgressChallenges () {
+      return challenges.slice(0, 3) || []
+    },
+    newChallenges () {
+      return this.challenges.slice(3) || []
+    },
+    solvedChallenges () {
+      return this.challenges.slice(0, 6) || []
+    }
+  },
   mounted () {
     this.$store.dispatch('fetchChallenges')
     this.$store.dispatch('fetchUser')
